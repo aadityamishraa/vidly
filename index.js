6046412b7ed8c6b09178e4264166e4bbc39de175
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-08 07:26:11 +0530

    put and delete req added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/api/genres', (req, res) => {
     res.send(genre);
 });
 
-app.put('/:id', (req, res) => {
+app.put('/api/genres/:id', (req, res) => {
     const genre = genres.find(c=> c.id===parseInt(req.params.id));
     if(!genre) return res.status(400).send('The genre with the given ID was not found.');
 
@@ -37,3 +37,30 @@ app.put('/:id', (req, res) => {
     genre.name = req.body.name;
     res.send(genre);
 });
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+    if (!genre) return res.status(404).send('The genre with the given ID was not found');
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+    
+    res.send(genre);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c=> c.id===parseInt(req.params.id));
+
+    if(!genre) return res.status(404).send('The genre with the given ID was not found.');
+    res.send(genre);
+});
+
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required
+    };
+
+    return Joi.validate(genre, schema);
+}
