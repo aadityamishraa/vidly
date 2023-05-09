49afc55910a5826762f099a4b72d32093fe1de9b
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-09 07:29:38 +0530

    connected to the database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
@@ -7,7 +8,9 @@ const genres = require('.routes/genres');
 app.use(express.json());
 app.use('/api/genres', genres);
 
-
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to the Database...'))
+    .catch((error) => console.error(`Couldn't connect to the database...`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
