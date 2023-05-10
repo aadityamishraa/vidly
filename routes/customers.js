df22c8fa19e37c75d76c7da97dddd6a13b27e6ce
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-10 07:01:30 +0530

    customers router modified

--- routes/customers.js
+++ routes/customers.js
@@ -3,31 +3,41 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const genreSchema = new mongoose.Schema({
+const customerSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50,
+    },
+    isGold: {
+        type: Boolean,
+        default: false,
+    },
+    phone: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 15,
     }
 });
 
-const Genre = mongoose.model('Genre', genreSchema);
+const Customers = mongoose.model('Genre', customerSchema);
 
 
 router.get('/', async (req, res) => {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
+    const customers = await Genre.find().sort('name');
+    res.send(customers);
 });
 
 router.post('/', async (req, res) => {
-    const { error } = new Joi.ValidationError(req.body, genreSchema);
+    const { error } = new Joi.ValidationError(req.body, Customers);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let genre = new Genre({ name: req.body.name });
+    let Customers = new Customers({ name: req.body.name });
 
-    genre = await genre.save();
-    res.send(genre);
+    Customers = await Customers.save();
+    res.send(Customers);
 });
 
 module.exports = router;
