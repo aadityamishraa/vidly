e40b5d2117b81509156728501f9356d76c7f086f
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-11 23:20:48 +0530

    movies put api created

--- routes/movies.js
+++ routes/movies.js
@@ -29,3 +29,27 @@ router.post('/', async (req, res) =>{
 
     res.send(movie);
 });
+
+router.put('/:id', async (req, res) =>{
+    const {error} = new Joi.ValidationError(req.body, movieSchema);
+
+    if(error) return res.send(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+
+    if(!genre) return res.send(400).send('Invalid genre.');
+
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        genre:{
+            _id: genre._id,
+            name: genre.name,
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate,
+    }, {new: true});
+
+    if(!movie) return res.send(404).send('The Movie with the given ID was not found.');
+
+    res.send(movie);
+});
