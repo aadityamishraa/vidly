6327314df5c4f984af638bbcff9a1a4fa3dbebb3
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-11 23:42:11 +0530

    rentals api added

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies= require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to the Database...'))
