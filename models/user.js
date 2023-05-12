adeb12fa9f28c3918960057478ee1e63e96284f7
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-12 07:06:31 +0530

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,23 +1,38 @@
 const mongoose = require('mongoose');
-
+const Joi = require('joi');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minlength: 5,
+        maxlength: 50,
     },
     email: {
         type: String,
         unique: true,
         required: true,
+        minlength: 5,
+        maxlength: 255,
     },
     password: {
         type: String,
         required: true,
-        minlength: 8,
-        maxlength: 20,
+        minlength: 5,
+        maxlength: 1024,
     }
 });
 
 const User = new mongoose.model('Users', userSchema);
 
-exports.User=User;
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).required,
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
