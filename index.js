b357a5d225489a646d95b2db0fdc776a6bf9ca5f
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-15 06:41:51 +0530

    auth file created and route added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+Joi.objectId = require('Joi.objectId')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
@@ -7,6 +9,7 @@ const customers = require('./routes/customers');
 const movies= require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 app.use(express.json());
 app.use('/api/genres', genres);
@@ -14,6 +17,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 
 mongoose.connect('mongodb://localhost/vidly')
