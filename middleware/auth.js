d522545ac7b36b8b9d6665442920c2af96a7120b
Aaditya Mishra <aadityamishra216@gmail.com>
2023-05-20 08:32:00 +0530

    modified middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,14 @@ const config = require('config');
 const jwt = require('jwtwebtoken');
 
 
-function auth(req, res, next){
+module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
-    if(!token) res.send(401).send('Access denied. No token provided');
+    if (!token) return res.send(401).send('Access denied. No token provided');
 
     try {
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-        res.user=decoded;
+        res.user = decoded;
+        next();
     } catch (ex) {
         res.status(400).send('Invalid token');
     }
